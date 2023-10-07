24a12349e52ef7f6ec82ecbc61cb979b4935875c
Marc Geremias Serra <marcgeremias5@gmail.com>
2023-10-07 15:31:27 +0200

    WIP - Fixed ParallelSearch adding AscendantSearch and DescendantSearch classes - Finished ParallelSearchArray using a new class named SearchBetween - Added comment with already tested numbers

--- Exercise1/src/ParallelSearch.java
+++ Exercise1/src/ParallelSearch.java
@@ -2,60 +2,18 @@ import java.util.LinkedList;
 import java.util.Scanner;
 
 public class ParallelSearch {
-    private static final int MAX_SIZE = 10;
-    private final LinkedList<Integer> list;
-    private final int target;
-    private boolean foundByFirstThread = false;
-    private boolean foundBySecondThread = false;
-
-    public ParallelSearch(LinkedList<Integer> list, int target) {
-        this.list = list;
-        this.target = target;
-    }
-    public void searchFromBeginning() {
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i) == target) {
-                foundByFirstThread = true;
-                break;
-            }
-        }
-    }
-    public void searchFromEnd() {
-        for (int i = list.size() - 1; i >= 0; i--) {
-            if (list.get(i) == target) {
-                foundBySecondThread = true;
-                break;
-            }
-        }
-    }
+    private static final int MAX_SIZE = 1000;
     public static void main(String[] args) throws InterruptedException {
-        LinkedList<Integer> myList = new LinkedList();
+        LinkedList<Integer> myList = new LinkedList<>();
 
-        for (int i = 0; i < MAX_SIZE; i++) {
-            myList.add(i);
-        }
+        for (int i = 0; i < MAX_SIZE; i++) myList.add(i);
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the number to search for: ");
-        int numberToFind = scanner.nextInt();
+        System.out.print("Enter the number to search for: ");
+        int toSearch = new Scanner(System.in).nextInt();
 
-        ParallelSearch parallelSearch = new ParallelSearch(myList, numberToFind);
-
-        Thread thread1 = new Thread(() -> parallelSearch.searchFromBeginning());
-        Thread thread2 = new Thread(() -> parallelSearch.searchFromEnd());
-
-        thread1.start();
-        thread2.start();
-        thread1.join();
-        thread2.join();
-
-        if (parallelSearch.foundByFirstThread) {
-            System.out.println("Number found by the 1st thread.");
-        } else if (parallelSearch.foundBySecondThread) {
-            System.out.println("Number found by the 2nd thread.");
-        } else {
-            System.out.println("Number not found in the list.");
-        }
-
+        AscendantSearch ascendantSearch = new AscendantSearch(myList, toSearch);
+        DescendantSearch descendantSearch = new DescendantSearch(myList, toSearch);
+        descendantSearch.start();
+        ascendantSearch.start();
     }
 }
--- Exercise1/src/ParallelSearchArray.java
+++ Exercise1/src/ParallelSearchArray.java
@@ -1,32 +1,69 @@
-import java.util.LinkedList;
 import java.util.Scanner;
 
 public class ParallelSearchArray {
-    private static final int MAX_SIZE = 10;
-    public static int ParallelSearch (int toSearch, int[] array, int numThreads){
-        int result = -1;
+    private static final int MAX_SIZE = 1000;
+    private static final int NO_RESULT_FOUND = -1;
+    private static int numThreads;
+    private static int toSearch;
+    private static int[] array;
 
+    public static int parallelSearch () throws InterruptedException {
+        SearchBetween[] threads = new SearchBetween[numThreads];
         int subArraySize = array.length / numThreads;
+        int to = subArraySize;
+        int from = 0;
 
-
-        return result;
-    }
-    public static void main(String[] args) throws InterruptedException {
-        Scanner scanner = new Scanner(System.in);
-
-        int[] myArray = new int[10];
-        for (int i = 0; i < MAX_SIZE; i++) {
-            myArray[i] = i;
+        // Start all threads
+        for (int i = 0; i < numThreads; i++) {
+            threads[i] = new SearchBetween(array, toSearch, from, to, i);
+            threads[i].start();
+            from = to;
+            to += subArraySize;
         }
 
-        System.out.println("Enter the number to search for: ");
-        int numberToFind = scanner.nextInt();
+        // Wait for all threads to finish to get the return value
+        for (int i = 0; i < numThreads; i++) {
+            threads[i].join();
+            // Check if any thread found the number
+            if (threads[i].getResult() != -1) {
+                System.out.println("[" + i + "] Found at index " + threads[i].getResult());
+                return threads[i].getResult();
+            }
+        }
+        return NO_RESULT_FOUND;
+    }
+    public static void main(String[] args) throws InterruptedException {
+        array = new int[MAX_SIZE];
+        for (int i = 0; i < MAX_SIZE; i++) array[i] = i;
 
-        System.out.println("Enter the number of threads to open: ");
-        int numThreads = scanner.nextInt();
+        System.out.print("Enter the number to search for: ");
+        toSearch = new Scanner(System.in).nextInt();
+        System.out.print("Enter the number of threads to open: ");
+        numThreads = new Scanner(System.in).nextInt();
 
-        int result = ParallelSearch(numberToFind, myArray, numThreads);
-        System.out.println("Result: " + result);
+        parallelSearch();
+
+        /*
+            1000 (5 Threads)
+            0-200	= 0
+            200-400	= 1
+            400-600	= 2
+            600-800 = 3
+            800-1000 = 4
+
+            1000 (20 Threads)
+            0-50	= 0
+            50-100	= 1
+            100-150	= 2
+            150-200	= 3
+            200-250	= 4
+            250-300	= 5
+            300-350	= 6
+            350-400	= 7
+            400-450	= 8
+            450-500	= 9
+            ...
+         */
     }
 
 }
