24cb7bd5790d7d8dc29adc75fae2edabe51ce835
Guillem <gody08112002@gmail.com>
2023-10-31 13:52:29 +0100

    first functions from mutex

--- Exercise2/src/LamportMutex.java
+++ Exercise2/src/LamportMutex.java
@@ -3,14 +3,30 @@ import java.util.Queue;
 
 public class LamportMutex implements Runnable{
     LamportClock lamportClock;
-    PriorityQueue<Integer> priorityQueue;
+    PriorityQueue<Integer> requestList;
+    private boolean requestMade;
+
+
+    public synchronized void queueRequest(int request) {
+        requestList.add(request);
+    }
+    public synchronized void releaseRequest() {
+        requestMade = false;
+        requestList.remove(0);
+    }
+
+
+    /*
+    public void accesCS() {
+        lamportClock.sendAction();
+        requestList.add(lamportClock.getValue());
+    }
+     */
 
     @Override
     public void run() {
-        priorityQueue = new PriorityQueue<>();
+        requestList = new PriorityQueue<>();
         lamportClock = new LamportClock();
-
-
 
     }
 }
