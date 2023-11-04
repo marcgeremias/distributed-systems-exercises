0a960b31e5c757e1b0e0a62379c921654bff2cda
Marc Geremias Serra <marcgeremias5@gmail.com>
2023-11-04 18:23:27 +0100

    WIP: - Modified to priorityQ

--- Exercise2/src/Lightweight.java
+++ Exercise2/src/Lightweight.java
@@ -2,11 +2,24 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicIntegerArray;
 
 public class Lightweight extends Thread{
     LamportClock clock;
-    int[] requestQ;
-    int numOkay;
+    //int[] requestQ;
+    //AtomicIntegerArray requestQ;
+    private final PriorityQueue<Message> queue = new PriorityQueue<>((r1, r2) -> {
+        int compare = Integer.compare(r1.getTimestamp(), r2.getTimestamp());
+        if (compare == 0) {
+            return Integer.compare(r1.getSrcId(), r2.getSrcId());
+        }
+        return compare;
+    });
+    AtomicInteger numOkay;
+    //int numOkay;
 
     private static final int NUM_LIGHTWEIGHTS = 3;
     private static final int NUM_PRINTS = 10;
@@ -15,19 +28,26 @@ public class Lightweight extends Thread{
     private int heavyPort;
     private int myPort;
     private int myId;
-    private boolean start;
+    //private boolean start;
+    private AtomicBoolean start;
 
     public Lightweight(int myId, int heavyPort, int myPort, ArrayList<Integer> portsWithoutMe) {
         this.lightweightPorts = portsWithoutMe;
         this.heavyPort = heavyPort;
         this.myPort = myPort;
         this.myId = myId;
-        this.start = false;
+        //this.start = false;
+        this.start = new AtomicBoolean(false);
 
-        this.requestQ = new int[NUM_LIGHTWEIGHTS];
+        //this.requestQ = new int[NUM_LIGHTWEIGHTS];
+        //this.requestQ = new AtomicIntegerArray(NUM_LIGHTWEIGHTS);
+
         this.clock = new LamportClock();
-        this.numOkay = 0;
-        for (int i = 0; i < NUM_LIGHTWEIGHTS; i++) requestQ[i] = Integer.MAX_VALUE;
+        //this.numOkay = 0;
+        this.numOkay = new AtomicInteger(0);
+        //for (int i = 0; i < NUM_LIGHTWEIGHTS; i++) requestQ[i] = Integer.MAX_VALUE;
+        //for (int i = 0; i < NUM_LIGHTWEIGHTS; i++) requestQ.set(i, Integer.MAX_VALUE);
+
     }
 
     private void notifyHeavyWeight() {
@@ -35,23 +55,28 @@ public class Lightweight extends Thread{
     }
 
     private void waitHeavyWeight() {
-        while(!getStart());
+        //while(!getStart());
+        while(!start.get());
     }
 
-    private synchronized boolean getStart() {
+    /*private synchronized boolean getStart() {
         return this.start;
     }
 
     private synchronized void setStart(boolean start) {
         this.start = start;
-    }
+    }*/
 
 
-    private synchronized void requestCS() {
+    private void requestCS() {
         clock.tick();
-        requestQ[myId] = clock.getValue();
-        broadcastMsg(new Message(myId, myPort, Message.MESSAGE_REQUEST, requestQ[myId]));
-        while(!okayCS());
+        //requestQ.set(myId, clock.getValue());
+        Message msg = new Message(myId, myPort, Message.MESSAGE_REQUEST, clock.getValue());
+        synchronized (queue) {
+            queue.add(msg);
+        }
+        broadcastMsg(msg);
+        while(!this.okayCS());
     }
 
     private void broadcastMsg(Message msg) {
@@ -61,34 +86,31 @@ public class Lightweight extends Thread{
     }
 
     private void releaseCS() {
-        clock.tick();
-        requestQ[myId] = Integer.MAX_VALUE;
-        broadcastMsg(new Message(myId, myPort, Message.MESSAGE_RELEASE, requestQ[myId]));
-        numOkay = 0;
-    }
-    private synchronized boolean okayCS() {
-        if(numOkay < NUM_LIGHTWEIGHTS - 1) return false;
-        for(int i = 0; i < NUM_LIGHTWEIGHTS; i++){
-            if(isGreater(requestQ[myId], myId, requestQ[i], i)){
-                return false;
-            }
-            if(isGreater(requestQ[myId], myId, clock.getValue(), i)){
-                return false;
-            }
+        //clock.tick();
+        //requestQ.set(myId, Integer.MAX_VALUE);
+        //queue.removeIf(msg -> msg.getSrcId() == myId);
+        synchronized (queue) {
+            queue.removeIf(msg -> msg.getSrcId() == myId);
         }
-        return true;
+        broadcastMsg(new Message(myId, myPort, Message.MESSAGE_RELEASE, clock.getValue()));
+        numOkay.set(0);
+    }
+    private boolean okayCS() {
+        if (numOkay.get() < NUM_LIGHTWEIGHTS - 1) return false;
+        //return !queue.isEmpty()
+        //        && queue.peek().getSrcId() == myId;
+        synchronized (queue) {
+            return !queue.isEmpty()
+                    && queue.peek().getSrcId() == myId;
+        }
     }
 
-    private boolean isGreater(int entry1, int pid1, int entry2, int pid2) {
-        if(entry2 == Integer.MAX_VALUE) return false;
-        return ((entry1 > entry2) || ((entry1 == entry2) && (pid1 > pid2)));
-    }
 
     private void listenServerConnections() {
         while(true){
             String line = Message.getMsg(serverSocket);
             if (line.equals(Message.MESSAGE_PRINT)) {
-                this.setStart(true);
+                this.start.set(true);
                 System.out.println("Lightweight " + myId + " ready to start");
             } else {
                 Message msg = new Message(line);
@@ -98,14 +120,24 @@ public class Lightweight extends Thread{
     }
 
     private void handleMsg(Message msg) {
-        clock.receiveAction(msg.getTimestamp());
+        //clock.receiveAction(msg.getTimestamp());
         if(msg.getTag().equals(Message.MESSAGE_REQUEST)){
-            requestQ[msg.getSrcId()] = msg.getTimestamp();
+            //requestQ.set(msg.getSrcId(), msg.getTimestamp());
+            //queue.add(msg);
+            clock.receiveAction(msg.getTimestamp());
+            synchronized (queue) {
+                queue.add(msg);
+            }
             Message.sendMsg(new Message(myId, myPort, Message.MESSAGE_ACK, clock.getValue()), msg.getSrcPort());
         }else if(msg.getTag().equals(Message.MESSAGE_ACK)){
-            numOkay++;
+            clock.receiveAction(msg.getTimestamp());
+            numOkay.getAndIncrement();
         } else if(msg.getTag().equals(Message.MESSAGE_RELEASE)){
-            requestQ[msg.getSrcId()] = Integer.MAX_VALUE;
+            //requestQ.set(msg.getSrcId(), Integer.MAX_VALUE);
+            //queue.removeIf(m -> m.getSrcId() == msg.getSrcId());
+            synchronized (queue) {
+                queue.removeIf(m -> m.getSrcId() == msg.getSrcId());
+            }
         }
     }
 
@@ -126,10 +158,10 @@ public class Lightweight extends Thread{
             waitHeavyWeight();
             requestCS();
             for (int i=0; i<NUM_PRINTS; i++){
-                System.out.println("I'm lightweight process " + myId);
+                System.out.println("I'm lightweight process " + myId + queue);
+                Heavyweight.waitSecond();
             }
-            Heavyweight.waitSecond();
-            this.setStart(false);
+            this.start.set(false);
             releaseCS();
             notifyHeavyWeight();
         }
