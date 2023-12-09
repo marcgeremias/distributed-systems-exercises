c1f26958d77c607f44ed7f22cf632248006ae8d0
Marc Geremias Serra <marcgeremias5@gmail.com>
2023-12-09 14:11:38 +0100

    WIP: - Added WebSocketServer and WebSocketServerEndpoint for testing porpouses - Added Maven framework to be able to add dependencies in pom.xml - Added the corresponding depdendecies

---
+++ Exercise4/src/WebSocketServerEndpoint.java
@@ -0,0 +1,34 @@
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+@ServerEndpoint(value = "/demoApp")
+public class WebSocketServerEndpoint {
+    @OnOpen
+    public void onOpen (Session session) {
+        System.out.println("[SERVER]: Handshake successful!!!!! - Connected!!!!! - Session ID: " + session.getId());
+        // Send message to client
+        for (int i = 0; i < 100; i++) {
+            try {
+                session.getBasicRemote().sendText("Hello from server - " + i);
+                Thread.sleep(1000);
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    @OnMessage
+    public String onMessage (String message, Session session) {
+        if (message.equalsIgnoreCase("terminate")) {
+            try {
+                session.close(new CloseReason(CloseReason.CloseCodes.NORMAL_CLOSURE, "Successfully session closed....."));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return message;
+    }
+    @OnClose
+    public void onClose (Session session, CloseReason closeReason) {
+        System.out.println("[SERVER]: Session " + session.getId() + " closed, because " + closeReason);
+    }
+}
